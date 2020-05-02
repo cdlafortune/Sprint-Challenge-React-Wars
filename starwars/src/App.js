3826e8b8e21b294a59ba17d7a324377a4d7226df
cdsuddreth <cdsuddreth@gmail.com>
2020-05-01 21:06:58 -0400

    an attempt was made

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from "./components/Character";
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  // the state properties here.\
+
+  const [myData] = useState(null);
+
+
+  useEffect( () => {
+    axios.get("https://swapi.py4e.com/api/people/")
+    .then(response => {
+      console.log(response);
+      myData = response.data.results;
+      return myData;
+    })
+
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
+  
+  
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,8 +33,16 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {myData.map(el => {
+        return <Character 
+          name = {el.name}
+          height = {el.height}
+          mass = {el.mass}
+          birthYear = {el.birth_year}
+        />
+        })}
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,36 @@
-// Write your Character component here
+import React, {useState} from "react";
+import styled from "styled-components";
+
+export default function Character(props) {
+
+    //get character info from props
+    const name = props.name;
+    const height = props.height;
+    const mass = props.mass;
+    const birthYear = props.birthYear;
+
+    //style character card 
+    const characterCard = styled.div`
+        background-color: white;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        color: black;
+        font-family: monospace;
+    `;
+    const largeText = styled.h2`
+        font-size: 2rem;
+    `;
+
+
+    return (
+        <characterCard>
+            <largeText>Name: {name}</largeText>
+            <p>Birth Year: {birthYear}</p>
+            <p>Height: {height}</p>
+            <p>Mass: {mass}</p>
+        </characterCard>
+    );
+
+
+};
